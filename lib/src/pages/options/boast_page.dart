658eecac4e33eec94380252b26d285815ba2b906
Oliver Yunior Mendez <39278672+olivermendez@users.noreply.github.com>
2021-11-03 21:13:12 -0400

    Update boast_page.dart

--- lib/src/pages/options/boast_page.dart
+++ lib/src/pages/options/boast_page.dart
@@ -21,6 +21,66 @@ class BoastPageForm extends StatelessWidget {
         backgroundColor: Colors.black87,
         centerTitle: false,
       ),
+      body: addDynamic(item),
     );
+  }
+}
+
+addDynamic(String item) {
+  if (item == 'Power Boats') {
+    return const PowerBoastPage();
+  } else if (item == 'Sail Boats') {
+    return const SailBoastPage();
+  } else if (item == 'Personal Watercraft') {
+    return const PersonalWatercraftPage();
+  } else if (item == 'Boat Parts') {
+    return const BoastPartsPage();
+  } else {
+    return Container(
+      color: Colors.red,
+      child: const Center(
+        child: Text(
+          "Listing no defined",
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
+
+class PowerBoastPage extends StatelessWidget {
+  const PowerBoastPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class SailBoastPage extends StatelessWidget {
+  const SailBoastPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class BoastPartsPage extends StatelessWidget {
+  const BoastPartsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class PersonalWatercraftPage extends StatelessWidget {
+  const PersonalWatercraftPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
