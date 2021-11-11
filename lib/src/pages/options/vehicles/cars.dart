f19fe781967d778e003a9f93330a06d0240f7e71
Oliver Yunior Mendez <39278672+olivermendez@users.noreply.github.com>
2021-11-11 02:06:23 -0400

    Update cars.dart

--- lib/src/pages/options/vehicles/cars.dart
+++ lib/src/pages/options/vehicles/cars.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:my_app/config/constant.dart';
 import 'package:my_app/models/bodytypes.dart';
 import 'package:my_app/models/token.dart';
+
+import 'package:http/http.dart' as http;
 
 class New extends StatefulWidget {
   final Bodytype bodySeleted;
@@ -18,8 +23,6 @@ class New extends StatefulWidget {
 }
 
 class _New extends State<New> {
-  final GlobalKey<FormState> _key = GlobalKey<FormState>();
-
   String bodytype = '';
   String dimensions = '';
   String weight = '';
@@ -47,7 +50,7 @@ class _New extends State<New> {
           itemBuilder: (context, index) {
             final opt = widget.bodySeleted.details[index];
 
-            return typeCarSelectedImage(opt, widget.image);
+            return typeCarSelectedImage(opt, widget.image, widget.token);
           },
         ),
       ),
@@ -55,7 +58,7 @@ class _New extends State<New> {
   }
 }
 
-Widget typeCarSelectedImage(Detail opt, String image) {
+Widget typeCarSelectedImage(Detail opt, String image, Token token) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.stretch,
     children: [
@@ -101,79 +104,149 @@ Widget typeCarSelectedImage(Detail opt, String image) {
         ),
       ]),
       const Divider(),
-      carsForm()
+      carsForm(token, image)
     ],
   );
 }
 
-Widget carsForm() {
+Widget carsForm(Token token, String image) {
+  TextEditingController _title = TextEditingController();
+  TextEditingController _description = TextEditingController();
+  TextEditingController _quantity = TextEditingController();
+
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
   return Form(
+      key: _key,
       child: SingleChildScrollView(
-    child: Column(
-      children: [
-        TextFormField(
-          decoration: const InputDecoration(
-            label: Text('Shipment Title'),
-            helperText: 'e.g. 2015 Chevrolet Master',
-            hintStyle: TextStyle(fontSize: 13, color: Colors.grey),
-            filled: true,
-            isDense: true,
-          ),
-          onChanged: (value) {
-            // _title = value;
-          },
-          keyboardType: TextInputType.text,
-          autocorrect: false,
-        ),
-        const Divider(
-          height: 10,
-          color: Colors.white,
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            label: Text('Quantity'),
-            hintStyle: TextStyle(fontSize: 13, color: Colors.grey),
-            filled: true,
-            isDense: true,
-          ),
-          keyboardType: TextInputType.number,
-          autocorrect: false,
-        ),
-        const Divider(
-          height: 20,
-          color: Colors.white,
-        ),
-        TextFormField(
-          maxLines: 3,
-          decoration: const InputDecoration(
-            label: Text('Add description'),
-            helperText: 'Instructions you would like to give to your mover?',
-            hintStyle: TextStyle(fontSize: 13, color: Colors.grey),
-            filled: true,
-            isDense: true,
-          ),
-          onChanged: (value) {
-            //_description = value;
-          },
-          keyboardType: TextInputType.text,
-          autocorrect: false,
-        ),
-        const Divider(
-          height: 20,
-          color: Colors.white,
-        ),
-        Row(
+        child: Column(
           children: [
-            Expanded(
-              child: ElevatedButton(
-                  onPressed: () {
-                    //createListing(context);
-                  },
-                  child: const Text("Continue")),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "add title";
+                }
+                return null;
+              },
+              controller: _title,
+              decoration: const InputDecoration(
+                label: Text('Shipment Title'),
+                helperText: 'e.g. 2015 Chevrolet Master',
+                hintStyle: TextStyle(fontSize: 13, color: Colors.grey),
+                filled: true,
+                isDense: true,
+              ),
+              onChanged: (value) {
+                // _title = value;
+              },
+              keyboardType: TextInputType.text,
+              autocorrect: false,
+            ),
+            const Divider(
+              height: 10,
+              color: Colors.white,
+            ),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "add quantity";
+                }
+                return null;
+              },
+              controller: _quantity,
+              decoration: const InputDecoration(
+                label: Text('Quantity'),
+                hintStyle: TextStyle(fontSize: 13, color: Colors.grey),
+                filled: true,
+                isDense: true,
+              ),
+              keyboardType: TextInputType.number,
+              autocorrect: false,
+            ),
+            const Divider(
+              height: 20,
+              color: Colors.white,
+            ),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "add description";
+                }
+                return null;
+              },
+              controller: _description,
+              maxLines: 3,
+              decoration: const InputDecoration(
+                label: Text('Add description'),
+                helperText:
+                    'Instructions you would like to give to your mover?',
+                hintStyle: TextStyle(fontSize: 13, color: Colors.grey),
+                filled: true,
+                isDense: true,
+              ),
+              onChanged: (value) {
+                //_description = value;
+              },
+              keyboardType: TextInputType.text,
+              autocorrect: false,
+            ),
+            const Divider(
+              height: 20,
+              color: Colors.white,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        if (_key.currentState!.validate()) {}
+                        Conection().createListing(
+                            _title.text,
+                            _description.text,
+                            _quantity.text,
+                            'vehicles',
+                            'cars',
+                            'assets/bodytypes/$image',
+                            token);
+                      },
+                      child: const Text("Continue")),
+                ),
+              ],
             ),
           ],
         ),
-      ],
-    ),
-  ));
+      ));
+}
+
+class Conection {
+  Future<void> createListing(
+      String _title,
+      String _description,
+      String _quantity,
+      String _comodity,
+      String _subcomodity,
+      String imageRoute,
+      Token token) async {
+    Map<String, dynamic> request = {
+      "title": _title,
+      "description": _description,
+      "quantity": _quantity,
+      "commodity": _comodity,
+      "subcomodity": _subcomodity,
+      "photo": imageRoute
+    };
+
+    //var url = Uri.parse('${Constants.apiUrl}listings/${token.user.id}/vehicle');
+    var url = Uri.parse('${Constants.apiUrl}listings');
+
+    var response = await http.post(
+      url,
+      body: jsonEncode(request),
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        'Authorization': 'Bearer ${token.token}'
+      },
+    );
+    var body = response.body;
+  }
 }
