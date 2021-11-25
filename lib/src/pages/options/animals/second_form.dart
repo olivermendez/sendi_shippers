107cb3510709c7056345a7227bb541a60d4195ae
Oliver Yunior Mendez <39278672+olivermendez@users.noreply.github.com>
2021-11-25 19:58:39 -0400

    Update second_form.dart

--- lib/src/pages/options/animals/second_form.dart
+++ lib/src/pages/options/animals/second_form.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/models/listing/listing.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:my_app/models/token.dart';
+
+const double _minHeight = 200;
 
 class SecondFormLocationAnimals extends StatefulWidget {
   //final Listing listingCreated;
@@ -14,99 +16,250 @@ class SecondFormLocationAnimals extends StatefulWidget {
 }
 
 class _SecondFormLocationAnimalsState extends State<SecondFormLocationAnimals> {
-  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+  //final _key = GlobalKey<FormState>();
+  LatLng startingLocation = LatLng(-2.1611081, -79.9022226);
+  GoogleMapController? _mapController;
+
+  void onCreated(GoogleMapController mapController) {
+    _mapController = mapController;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Pickup & Delivery"),
-        centerTitle: false,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          const SizedBox(
-            height: 30,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 20.0),
-            child: Text(
-              "You will move: ",
-              style: TextStyle(fontWeight: FontWeight.bold),
+          Padding(
+            padding: const EdgeInsets.only(bottom: _minHeight),
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: startingLocation,
+                zoom: 15,
+              ),
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              zoomControlsEnabled: false,
+              onMapCreated: onCreated,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Form(
-                key: _key,
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Pick up location',
-                          filled: true,
-                          isDense: true,
-                          //icon: Icon(
-                          //Icons.location_on,
-                          //size: 30,
-                          //)
-                        ),
-                        keyboardType: TextInputType.streetAddress,
-                        autocorrect: false,
-                      ),
-                      const Divider(
-                        height: 30,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Delivery location',
-                          filled: true,
-                          isDense: true,
-                          /* icon: Icon(
-                        Icons.where_to_vote,
-                        size: 30,
-                      ) */
-                        ),
-                        keyboardType: TextInputType.streetAddress,
-                        autocorrect: false,
-                      ),
-                      const Divider(
-                        height: 40,
-                      ),
-                      const Text("When do you need your shitment deliver?",
-                          style: TextStyle(fontSize: 17)),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          //labelText: 'Delivery location',
-                          filled: true,
-                          isDense: true,
-                          hintText: 'M/D/YYYY',
-                          /*  icon: Icon(
-                        Icons.schedule,
-                        size: 30,
-                      ) */
-                        ),
-                        keyboardType: TextInputType.datetime,
-                        autocorrect: false,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 30.0),
-                        child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.pushNamedAndRemoveUntil(
-                                  context, 'home', (route) => false,
-                                  arguments: widget.token);
-                            },
-                            child: const Text("Continue")),
-                      )
-                    ],
+          Positioned(
+            child: FloatingActionButton(
+              mini: true,
+              backgroundColor: Colors.white,
+              onPressed: () {},
+              child: Icon(
+                Icons.gps_fixed,
+                color: Colors.black,
+              ),
+            ),
+            bottom: MediaQuery.of(context).size.height,
+          ),
+          Positioned(
+              top: 60,
+              left: 20,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.menu,
+                    color: Colors.black,
                   ),
-                )),
+                ),
+              )),
+          Positioned(
+            child: _draggable(),
+            height: _minHeight,
+            bottom: 0,
+            right: 0,
+            left: 0,
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(height: 180, child: _appBar()),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _appBar() {
+    return AppBar(
+      iconTheme: IconThemeData(color: Colors.black),
+      title: Text(
+        'Enter destination',
+        style: TextStyle(fontSize: 18, color: Colors.black),
+      ),
+      backgroundColor: Colors.white,
+      leading: IconButton(
+        icon: Icon(Icons.close),
+        onPressed: () {},
+      ),
+      bottom: PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 21.5),
+          child: Column(
+            children: [
+              _Input(
+                  iconData: Icons.gps_fixed,
+                  hintText: 'Jose Santiago Castillo 28',
+                  color: Colors.green),
+              SizedBox(
+                height: 9,
+              ),
+              Row(
+                children: [
+                  _Input(
+                    iconData: Icons.place_sharp,
+                    color: Colors.indigo,
+                    hintText: 'Ingresar destino',
+                  ),
+                  Icon(
+                    Icons.add,
+                    size: 25,
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _draggable() {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black54,
+              offset: Offset(0, -1),
+              blurRadius: 3,
+            ),
+          ],
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15.0),
+            topRight: Radius.circular(15.0),
+          )),
+      child: Column(children: [
+        Container(
+          margin: EdgeInsets.all(10.0),
+          width: 35,
+          color: Colors.grey[300],
+          height: 3.5,
+        ),
+        _searchButton(),
+        LocationListTile(head: 'Enter Home Location', icon: Icons.home),
+        LocationListTile(head: 'Enter Work Location', icon: Icons.work),
+      ]),
+    );
+  }
+
+  Widget _searchButton() {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        height: 50,
+        width: MediaQuery.of(context).size.width / 1.1,
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(left: 10.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6.0), color: Colors.grey[200]),
+        child: Text(
+          'Where to?',
+          style: TextStyle(fontSize: 17.5, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+class LocationListTile extends StatelessWidget {
+  final String head;
+  final IconData icon;
+  const LocationListTile({Key? key, required this.head, required this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 37.0, top: 26.0),
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            color: Colors.grey,
+            size: 25,
+          ),
+          SizedBox(
+            width: 22,
+          ),
+          Text(
+            head,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// ignore: unused_element
+class _Input extends StatelessWidget {
+  final IconData? iconData;
+  final void Function(String)? onChanged;
+  final String? hintText;
+  final VoidCallback? ontap;
+  final bool? enabled;
+  final Color? color;
+  const _Input({
+    Key? key,
+    this.iconData,
+    this.onChanged,
+    this.hintText,
+    this.ontap,
+    this.enabled = false,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          iconData,
+          size: 19,
+          color: color,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Container(
+              height: 40,
+              width: MediaQuery.of(context).size.width / 1.4,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6.0),
+                color: Colors.grey[200],
+              ),
+              child: TextField(
+                  onTap: ontap,
+                  enabled: enabled,
+                  onChanged: onChanged,
+                  decoration: InputDecoration.collapsed(
+                      hintText: hintText,
+                      hintStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      )))),
+        )
+      ],
     );
   }
 }
